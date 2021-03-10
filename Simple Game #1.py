f053fe28f7e9085582e3b9469f168a2aa0b4ed29
NonHor1zon <77180931+NonHor1zon@users.noreply.github.com>
2021-03-10 21:15:13 +0700

    Add files via upload

---
+++ Simple Game #1.py
@@ -0,0 +1,240 @@
+#Game Pertama
+#Pengembang
+'''
+1. Muhammad Yahya Ramadhan
+2. Siapa?
+3. ...
+'''
+
+NamaPemain = input("Masukkan Namamu : ")
+SapaAwalMulai = "Halo"
+SapaAwalMulai2 = SapaAwalMulai + " " + NamaPemain
+print (SapaAwalMulai2)
+print ('Selamat datang di Game Buatanku')
+print ('Tolong Dimaklumi Baru Game Pertama')
+JawabMulai = "Start"
+JawabKeluar = "Exit"
+NilaiDasar = 0
+MasukanAwal = input ("Silahkan Memilih Antara, Start atau Exit : ")
+if MasukanAwal == JawabKeluar:
+    print ('Masih Dikembangkan')#Karena Syntax Error, Line Ini Dihapus. Line :" exit() "
+    quit
+elif MasukanAwal == JawabMulai:
+    print ('Mari Kita Mulai')
+    NilaiDasar + 1
+while (MasukanAwal != JawabMulai and JawabKeluar):
+    MasukanAwal = input ("Coba Lagi : ")
+if MasukanAwal == JawabKeluar:
+    print ('Masih Dikembangkan')
+elif MasukanAwal == JawabMulai:
+    print ('Mari Kita Mulai')
+    NilaiDasar + 1
+print ('Loading')
+print ('Loading')
+print ('Loading')
+print ('Loading')
+print ('Loading')
+print ('Loading')
+print ('Loading')
+print ('Loading')
+print ('Loading')
+print ('Loading')
+print ('Loading')
+print ('Loading')
+print ('Loading')
+Stamina = 100
+Kesehatan = 100
+Haus = 50
+Lapar = 50
+print ('Loading')
+print ('Loading')
+print ('Loading')
+print ('Loading')
+print ('Loading')
+print ('Loading')
+print ('Loading')
+print ('Loading')
+print ('Loading')
+print ('Loading')
+print ('Loading')
+LanjutkanDalam1SelesaiLoading = input("Tekan Tombol Enter Untuk Lanjut")
+print (' ')
+print (' ')
+print (' ')
+print (' ')
+print (' ')
+print (' ')
+print (' ')
+print (' ')
+print (' ')
+print (' ')
+print (' ')
+print (' ')
+print (' ')
+Jawab1 = "A" or "a"
+Jawab2 = "B" or "b"
+Jawab3 = "C" or "c"
+Jawab4 = "D" or "d"
+JawabYa = "Yes"
+JawabTidak = "No"
+print ('Kamu Menjelajahi Hutan')
+print ('Kamu Merasa Lelah Dan Haus')
+print ('Selain Itu Hari Sudah Sore')
+print ('Kamu Menemukan Reruntuhan Kuno di Tengah Hutan')
+print ('Apa Yang Akan Kamu Lakukan?')
+print ('A. Masuk Ke Reruntuhan')
+print ('B. Cari Sumber Air')
+print ('C. Menunggu Hingga Malam')
+print ('D. Membuat Tenda')
+Jawab_KetemuReruntuhan = input ("Jawab : ")
+if Jawab_KetemuReruntuhan == Jawab1 :
+    print("Kamu Memasuki Reruntuhan")
+    print("Tepat Setelah Kamu Memasukinya, Pintu Masuk Reruntuhan Tersebut Mulai Runtuh")
+    print("Namanya Juga Reruntuhan :)")
+elif Jawab_KetemuReruntuhan == Jawab2 :
+    print("Kamu Mencari Sumber Air, Namun Kamu Tidak Dapat Menemukannya")
+    print("Stamina -10")
+    Stamina -10
+    print("Kamu Memilih Untuk Memasuki Reruntuhan")
+    print("Seketika Setelah Kamu Masuk, Pintu Reruntuhan Mulai Runtuh")
+    print('Kamu Bergumam Dalam Hati "Ada Alasan Tempat Ini Disebut Reruntuhan"')
+elif Jawab_KetemuReruntuhan == Jawab3 :
+    print("Kamu Beristirahat Hingga Malam Tiba")
+    print("Kamu Tertidur Dengan Pulas Di Atas Rumput")
+    print("Ketika Kamu Bangun, Waktu Sudah Berjalan Sekitar 2 Jam")
+    print("Stamina + 20")
+    Stamina + 20
+    print("Pada Akhirnya Kamu Memutuskan Untuk Masuk Ke Dalam Reruntuhan")
+elif Jawab_KetemuReruntuhan == Jawab4 :
+    print("Kamu Memutuskan Untuk Membangun Tenda")
+    print("Namun Kamu Tidak Bisa Tidur")
+    print("Pikiranmu Terus Terpusat Pada Reruntuhan Itu")
+    print("Pada Akhirnya Kamu Memutuskan Untuk Memasuki Reruntuhan Itu")
+    print("Seketika Setelah Kamu Masuk, Pintu Reruntuhan Tersebut Mulai Runtuh")
+print('tekan ENTER untuk lanjut')
+EnterLanjut1 = input('')
+print("Kamu Memutuskan Untuk Melihat Kembali Pintu Masuk Yang Telah Runtuh Tadi")
+print("Reruntuhan Itu Jatuh Sempurna Menutupi Pintu Masuk")
+print('"Tak Bisa Dilewati" Pikirmu')
+print("Kamu Memperhatikan Nilai Statusmu")
+print("Kesehatan")
+print(Kesehatan)
+print("Stamina")
+print(Stamina)
+print("Haus")
+print(Haus)
+print("Lapar")
+print(Lapar)
+print("Tekan ENTER untuk lanjut")
+EnterLanjut2 = input('')
+print("Kamu melihat kondisi ruangan di sekitaarmu")
+print("Kamu melihat beberapa jalan")
+print("Satu diantaranya menuju ke ruangan bawah")
+print("Satu diantaranya menuju ke sebuah ruangan yang masih tampak bagus")
+print("Satu diantaranya menuju ke perpustakaan tua")
+print("Satunya lagi menuju ke semacam gudang")
+print("Kemanakah kamu akan pergi")
+print("A. Ruangan Bawah")
+print("B. Ruangan Yang Masih Bagus")
+print("C. Perpustakaan Tua")
+print("D. Gudang Tua")
+Jawab_DalamReruntuhan_1 = input ("Jawab : ")
+if Jawab_DalamReruntuhan_1 = Jawab1 :
+    print ("Kamu mula berjalan ke ruangan tersebut")
+    print ("Ruangan tersebut nampak berantakan")
+    print ("Kamu mulai melihat-lihat sekitar")
+    print ("Kamu melihat dua buah pintu di sisi yang saling berlawanan")
+    print ("Kemana kamu akan pergi?")
+    print ("A. Kiri")
+    print ("B. Kanan")
+    Jawab_BawahTanah_1 = input ("Jawab : ")
+    if Jawab_BawahTanah_1 = Jawab1 :
+        print('Kamu membuka pintu sebelah kiri')
+        print('Pintu tersebut terasa lumayan berat')
+        print('Kamu tetap berusaha, hingga pintu tersebut terbuka')
+        print('Kamu menemukan selembar perkamen tua yang sudah agak buram')
+        print('Perkamen tersebut dibuat dari bahasa yang tidak kamu ketahui')
+        print('Kamu mulai kembali melihat sekeliling')
+        print('Kamu menemukan senter')
+        print('Apakah kamu ingin mencoba menyalakannya?')
+        print('Yes/No')
+        NyalaSenter_1 = input ('Jawab : ')
+        if NyalaSenter_1 = JawabYa :
+            print('Kamu mencoba menyalakan senter tersebut')
+            print('Kamu mulai mencari baterai di ruangan tersebut')
+            print('Kamu berdiri di depan sebuah rak tua')
+            print('Kamu mengambil baterai dimeja dan memasangkannya ke senter')
+            print('Kamu menemukan sebuah peti kecil')
+            print('Apakah Kamu Ingin Membukanya?')
+            print('Yes/No')
+            PetiKecil_1 = input ('Jawab : ')
+            if PetiKecil_1 = JawabYa :
+                print('Kamu memutuskan untuk membuka peti tersebut')
+                print('Kamu mengerahkan seluruh tenagamu')
+                print('Tapi Peti Tersebut Tidak Terbuka')
+                print('Kamu merasa lelah dan tersandung sebuah benda')
+                print('Kamu melihat benda tersebut')
+                print('Kamu mengarahkan sentermu menuju benda tersebut')
+                print('Benda tersebut nampak seperti pipa namun agak pipih')
+                print('Ujung-ujungnya pipih dan tidak memiliki sudut')
+                print('Benda tersebut terasa sangat ringan walaupun nampak seperti besi')
+                print('Kamu berdiri dan menyadari pintu ruangan tersebut tertutup')
+                print('pintu tersebut tersangkut dengan kuat')
+                print('Kamu menyerah membukanya dan memilih untuk melihat sekitar')
+                print('Kamu menemukan dua jalan keluar')
+                print('Yang satu merupakan terowongan kecil yang ada di belakang sofa')
+                print('Sedangkan yang satu lagi merupakan sebuah jalan menuju lantai diatasnya')
+                print('Jalan mana yang kamu pilih?')
+                print('A. Atas, B. Bawah')
+                Keluar_BawahTanah_Kiri_1 = input ('Jawab : ')
+                if Keluar_BawahTanah_Kiri_1 = Jawab1 :
+                    print('Kamu memilih untuk pergi ke atas')
+                    print('Ada dua cara untuk pergi keatas')
+                    print('Yang pertama adalah menggunakan tangga')
+                    print('Yang kedua adalah menyusun barang-barang di ruangan tersebut untuk menjadi tangga')
+                    print('Apa yang akan kamu lakukan?')
+                    print('A. Gunakan Tangga')
+                    print('B. Susun Barang-Barang')
+                    Keluar_BawahTanah_Kiri_Naik = input ('Jawab : ')
+                    if Keluar_BawahTanah_Kiri_Naik = Jawab1 :
+                        print('Kamu mulai megangkat tangga yang berada di ujung ruangan')
+                        print('Entah kenapa kamu merasa tangga itu tidak seberat tangga pada umumnya')
+                        print('Kamu mulai memanjat tangga tersebut')
+                        print('Pada saat yang sama kamu mendengar suara langkah kaki dari arah luar ruangan')
+                        print('Keringat mulai menetes dari dahimu')
+                        print('Kamu segera naik keatas dan menutup lubang tempat kamu naik tadi')
+                    if Keluar_BawahTanah_Kiri_Naik = Jawab2 :
+                        print('Kamu memilih untuk memindahkan beberapa barang untuk naik keatas')
+                        print('Setelah kamu memindahkan rak, kamu menemukan sebuah laci dibaliknya')
+                        print('Kamu membuka laci tersebut dan menemukan sebuah kunci warna kuning')
+                        KunciKuning_1 = 1
+                        print('Kamu mendengar suara langkah kaki dari luar')
+                        print('Keringatmu mulai menetes')
+                        print('Kamu memutuskan untuk sembunyi di belakang rak buku')
+                        print('Kamu menutup diri agar tidak terlihat dari luar')
+                        print('Kamu mendengar pintu ruangan tersebut didobrak dari luar')
+                        print('Kamu segera mematikan senter dan diam bagaikan patung')
+                        print('Setelah beberapa saat kamu mulai tertidur')
+                    #
+                elif Keluar_BawahTanah_Kiri_1 = Jawab2 :
+                    print('Kamu memilih untuk melewati terowongan kecil')
+                #
+            elif PetiKecil_1 = JawabTidak :
+                print('Kamu berjalan keluar dari ruangan tersebut')
+                print('Kamu lantas bergerak menuju ruang di seberang')
+                #
+            #
+        elif NyalaSenter_1 = JawabTidak :
+            print('Kamu memasukkan senter tersebut kedalam tas')
+            print('Kamu terus menjelajahi ruangan itu dalam gelap')
+        #
+    elif Jawab_BawahTanah_1 = Jawab2 :
+        #
+    #
+elif Jawab_DalamReruntuhan_1 = Jawab2 :
+    #
+elif Jawab_DalamReruntuhan_1 = Jawab3 :
+    #
+elif Jawab_DalamReruntuhan_1 = Jawab4 :
+    #
+#Waaaa
